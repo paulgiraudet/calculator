0ff4d7d1593c714af44d95017fa64dcac0113701
Tictac <paul.giraudet@gmail.com>
2018-08-22 11:46:06 +0200

    fully fonctionnal calculator

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
+//input value
 var number = "";
-var number1= 0;
-var number2= 0;
+
+//saving value
+var number1= "";
+
+//saving operators
 var sum ="";
 
 //reinitialization
 function restart(){
   number = document.getElementById("display").innerHTML = "" ;
-  number1=0;
-  number2=0;
+  number1="";
   sum="";
 }
 
 //function +/-
 function inversion(){
-  number = document.getElementById("display").innerHTML = "-"+number ;
+  //need to be a number to work with -1 multiplication
+  number=Number(number);
+  number = document.getElementById("display").innerHTML = number*(-1);
 }
 
 //function suppr
 function suppr(){
+  //need to be a string to permit user deleting last character
+  number=number.toString();
   number = number.substring(0, number.length - 1);
-  number = document.getElementById("display").innerHTML = number;
+  document.getElementById("display").innerHTML = number;
 }
 
 //display number
@@ -27,30 +34,73 @@ function click_button(n){
   number = document.getElementById("display").innerHTML = number + n ;
 }
 
+//main operator system
 function operation(n){
-  number1 = Number(number);
-  number = "" ;
 
+  //saving first value
+  if (number1===""){
+    number1 = Number(number);
+  }
+
+  //comparing previous operator to make the operation correctly
+  else {
+    number = Number(number);
+    if (sum==="+"){
+      number=document.getElementById("display").innerHTML = number1 + number;
+    }
+    else if (sum==="-"){
+      number=document.getElementById("display").innerHTML = number1 - number ;
+    }
+    else if (sum==="x"){
+      number=document.getElementById("display").innerHTML = number1 * number ;
+    }
+    else if (sum==="/"){
+      number=document.getElementById("display").innerHTML = number1 / number ;
+    }
+    //saving new value
+    number1=number;
+  }
+
+  //reinitializing input value
+  number="";
+
+  //saving operator for the next operation
   sum = n ;
   return sum;
 }
 
+//only "=" button
 function result(){
-  
-  number2 = Number(number);
+
+  number = Number(number);
   if (sum==="+"){
-    number=document.getElementById("display").innerHTML = number1 + number2;
+    number=document.getElementById("display").innerHTML = number1 + number;
   }
   else if (sum==="-"){
-    number=document.getElementById("display").innerHTML = number1 - number2 ;
+    number=document.getElementById("display").innerHTML = number1 - number ;
   }
   else if (sum==="x"){
-    number=document.getElementById("display").innerHTML = number1 * number2 ;
+    number=document.getElementById("display").innerHTML = number1 * number ;
   }
   else if (sum==="/"){
-    number=document.getElementById("display").innerHTML = number1 / number2 ;
+    number=document.getElementById("display").innerHTML = number1 / number ;
   }
-  else if (sum==="%"){
-    number=document.getElementById("display").innerHTML = number1 % number2 ;
-  }
+
+  number1="";
+  sum="";
 }
+
+//function percentage
+function perc(){
+  number1=Number(number);
+  number=document.getElementById("display").innerHTML = number1 /100 ;
+}
+
+
+
+
+//garder l'opérateur et si la valeur du premier nombre est "" juste garder
+// l'opérateur en mémoire sinon faire l'opération directement
+//utiliser une variable de substitution qui est vide au depart et qui prend
+//la valeur du premier chiffre si et seulement si on a gardé l'opérateur en
+//mémoire sinon il prend la valeur de l'opération
